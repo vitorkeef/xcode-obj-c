0ef78c85a2129b6b6e4397f0c9bb8de04559a695
MakroTest <makrotest@ms2macbook.local>
2017-07-04 10:44:56 -0300

    Initial Commit

---
+++ l/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  l
+//
+//  Created by MakroTest on 04/07/17.
+//  Copyright © 2017 MakroTest. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ lTests/lTests.m
@@ -0,0 +1,39 @@
+//
+//  lTests.m
+//  lTests
+//
+//  Created by MakroTest on 04/07/17.
+//  Copyright © 2017 MakroTest. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface lTests : XCTestCase
+
+@end
+
+@implementation lTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
